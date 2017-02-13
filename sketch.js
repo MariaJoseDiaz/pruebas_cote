93cc00d60144f49fbbff26780191429c32381bae
MariaJoseDiaz <mjdiaz9@uc.cl>
2017-02-13 16:14:34 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,51 +1,47 @@
+
 var dots = [];
 var energy = 0; //starting value of energy
 var singleShake = 0;
-var finished = false;
 var maxEnergy= 1000; //max energy for eathquake
-
 var button1;
 var button2;
-var button3;
-var myChile;
-/*var myIndonesia;
-var myMexico;
-var myJapon;
-*/
-//var myResults;
+var cover;
 
-var fontTitle
+var myImage1;
+var myImage2;
 
+var fontTitle;
 
 function preload() {
-    //myResult = loadImage("images/prova1.png");
-    myChile = loadImage("chile.png");
-    fontTitle = loadFont("OCRBStd.otf");
+    cover = loadImage("images/Tavola disegno 18-100.jpg");
+    myImage1 = loadImage("images/prova1.png");
+    myImage2 = loadImage("images/prova2.jpg");
+      fontTitle = loadFont("OCRBStd.otf");
 }
     
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    //background(237, 34, 93);
-    angleMode(DEGREES);
-    
-     background(204);
-    
-    textFont(fontTitle);
-    textSize(height/20);
-    textAlign(CENTER);
-    textStyle(BOLD);
-    fill(0);
-    noStroke();
-    text("SHAKE YOUR DEVICE", width/2,height - height/1.1);   
 }
 
 function draw(){
-     
-    var magnitude = int(map(energy, 0, 1000, 0, 10)); 
+     background(204);
+     angleMode(DEGREES);  
+     var magnitude = int(map(energy, 0, 1000, 0, 10)); 
     
-    if (energy > 0 && energy < maxEnergy){
+     if (energy < 0.5) {
+     image(cover,0,0,windowWidth,windowHeight); 
+     } 
+    else if (energy > 0.5 && energy < maxEnergy){
         
-        //CREATE THE ELLIPSE AREA
+     textFont(fontTitle);
+     textSize(height/20);
+     textAlign(CENTER);
+     textStyle(BOLD);
+     fill(0);
+     noStroke();
+     text("EARTHQUAKE INTENSITY", width/2,height - height/1.1);
+        
+     //CREATE THE ELLIPSE AREA
     var x = width/2;
     var y = height/2;
     var r = energy * 2; 
@@ -57,12 +53,7 @@ function draw(){
 
     //magnitude indication
     fill(0);
-    noStroke();   
-        
-    textSize(height/20);
-    textAlign(CENTER);
-    textStyle(BOLD);
-    text("EARTHQUAKE INTENSITY", width/2,height - height/1.1);   
+    noStroke();    
     
     textSize(height/40);
     textAlign(CENTER);
@@ -73,119 +64,90 @@ function draw(){
     textAlign(CENTER);
     textStyle(BOLD);
     text(magnitude,width/2, height - height/6.7);
-    
-  /*  textSize(height/50);
-    textAlign(CENTER);
-    textStyle(NORMAL);    
-    text(energy, width/2, height - height/8); */
-        
-         //buttons  
-          
+           
     button1 = createButton("See results");
     button1.position(width/7,(height/15)*14);
     button1.touchStarted(results);
         
     button2 = createButton("Try again");
     button2.position((width/7)*5, (height/15)*14);
-    button2.touchStarted(clearEverything);
-    
-   // button3 = createButton('imagens');
-   // button3.position(width/3,height/3);
-   // button3.touchStarted(imagens);    
+    button2.touchStarted(clearEverything);   
         
-    } else if (energy > maxEnergy) {
-    //display things
-    background(0);
-  } //else {
-    //background(204);
-  //}
-      
-   
+    } else if(energy >= maxEnergy) {
+    textSize(height/20);
+    textAlign(CENTER);
+    textStyle(BOLD);
+    text("10",width/2, height - height/6.7)
+    energy = maxEnergy;
+    background(204,0,0);
+   }
+    
     //draw dots and given methods (actions)
       noStroke();
       fill(0);
       for (var i = 0; i < energy * 100; i++){
         dots[i].move();
-        dots[i]. display();
-        
+        dots[i]. display();  
       }
 }
 
 function deviceShaken(){
     
-    
    singleShake = abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
-  energy += singleShake;
-  
-  //  if (singleShake > 30){
-   //energy += singleShake;
-  //} else { finished == true;}
-    
+   energy += singleShake; 
    
     //create objects
     for (var i = 0; i < energy*100; i++){
         dots.push(new QuakeDots());
     } 
-    background(204);
-
+    
 }
-// HACER UN IF PARA CUANDO ES MENOR, PONER TEXTO PARA QUE LO INTENTE DE NUEVO CON MAS FUERZA
-
+    
 function QuakeDots(){ 
     var a = random(0,360);
-    var b = random(0,energy * 2);
+    var b = random(0,energy * 1.6);
     var x = sin(a) * b; // mi dà un numero che va da -b a b
     var y = cos(a) * b; // mi dà un numero che va da -b a b
     var d = dist(width/2,height/2, width/2, height/2 + x/2);
     
-    //var d = dist(width/2,height/2,width/2+value,height/2);
-    
     this.xdot = random(width/2 - d, width/2 + d); //according to ellipse area
     this.ydot = random(height/2 - d, height/2 + d); //according to ellipse area
-    this.diameter = 3;
+    this.diameter = 6;
     this.speed = 4; //according to magnitude
-        
 
-this.move = function(){
+    this.move = function(){
     this.xdot += random(-this.speed,this.speed);
     this.ydot += random(-this.speed,this.speed);
  
-}
+    }
 
-this.display = function(){
+    this.display = function(){
     if(this.xdot > width/2 + d || this.xdot < width/2 - d || this.ydot > height/2 + d || this.ydot < height/2 - d){
        this.xdot = random(width/2 - d, width/2 + d);
        this.ydot = random(height/2 - d, height/2 + d); 
        }
     ellipse(this.xdot, this.ydot, this.diameter, this.diameter);
-};
+    };
  
 }
-    
-    
+
     // result buttons
  function results() {
-     image(myChile,0,0,windowWidth,windowHeight);
+     if (magnitude <= 6){
+         image(myImage1,0,0,windowWidth,windowHeight);
+     } else {
+         image(myImage2,0,0,windowWidth,windowHeight);
+     }
   
  }
 
  function clearEverything() {
     background(204);
     energy = 0;
-    textSize(height/20);
-    textAlign(CENTER);
-    textStyle(BOLD);
-    fill(0);
-    noStroke();
-    text("SHAKE YOUR DEVICE", width/2,height - height/1.1);   
- 
  }
 
 function windowResized(){
     resizeCanvas(windowWidth,windowHeight);
-     }
+  }
 
- function touchMoved() {
-     // do some stuff
-      return false;
-}
+
